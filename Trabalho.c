f70d4a8466790cfb22f40c03faeb8fc8329efa2b
PAULO VICTOR DE CAMPOS BARBOSA <paulo.vbarbosa@a.ucb.br>
2024-11-13 10:53:14 -0300

    somar e multiplicar

--- Trabalho.c
+++ Trabalho.c
@@ -2,16 +2,50 @@
 #include <stdlib.h>
 
 diminuir(int valor1, int valor2){
-int dificuldade, result, resposta, i;
-        printf("Subtração");
-            printf("Digite o resultado da operação: %d - %d = ",valor1, valor2);
+int result, resposta, i;
+        printf("Subtração\n");
+            printf("Digite o resultado da operação: %d - %d = \n",valor1, valor2);
             scanf("%d",resposta);
             result = valor1 - valor2;
-        if (result == resposta)
+        if (result = resposta)
         {
-            printf("Respota correta.");
-            printf("+10 Pontos");
+            printf("Respota correta.\n");
+            printf("+10 Pontos\n");
         }else{
-            printf("Respota incorreta.");
+            printf("Respota incorreta.\n");
         }
+}
+somar(int valor1,int valor2){ 
+int resposta, resultado; 
+        printf("Adição\n"); 
+            printf("Digite o resultado da operação: %d + %d = \n", valor1, valor2); 
+            // Resposta do usuário 
+            scanf("%d", resposta); 
+            // Resultado 
+            resultado = valor1 + valor2; 
+            // Validação do resultado 
+            if (resultado = resposta)
+            { 
+                printf("Resposta correta\n");
+                printf("+10 Pontos\n");
+                } else { 
+                printf("Resposta incorreta\n"); 
+                } 
+}
+multiplicar(int valor1, int valor2){
+int resposta, resultado; 
+        printf("Multiplicação\n"); 
+            printf("Digite o resultado da operação: %d * %d = \n", valor1, valor2); 
+            // Resposta do usuário 
+            scanf("%d", resposta); 
+            // Resultado 
+            resultado = valor1 * valor2; 
+            // Validação do resultado 
+            if (resultado = resposta)
+            { 
+                printf("Resposta correta\n");
+                printf("+10 Pontos\n");
+                } else { 
+                printf("Resposta incorreta\n"); 
+                } 
 }
